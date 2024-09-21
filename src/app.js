f5f01e50d4c89123ce7b0978e59878d9cc5dd85d
Fikri Muhammad Fadhilah <179136498+fikrimf280305@users.noreply.github.com>
2024-09-21 11:11:42 +0700

    Add API Endpoints

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
+const { MongoClient } = require('mongodb')
+
 const express = require('express')
-
-const http2 = require('http2')
-
-const fs = require('fs')
-
-const path = require('path')
 
 const cors = require('cors')
 
-const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+const DATABASE_URL = process.env.DATABASE_URL
+
+const PORT = process.env.PORT
+
+const HOST = process.env.HOST
+
+const client = new MongoClient(DATABASE_URL)
 
 const app = express()
 
@@ -18,53 +22,76 @@ app.use(cors(), (req, res, next) => {
 
 })
 
-// Middleware untuk mengukur waktu eksekusi
-app.use((req, res, next) => {
-
-    const start = Date.now()
-  
-    // Menangkap metode res.json agar kita bisa memodifikasi respons
-    const originalJson = res.json
-  
-    res.json = (data) => {
-
-        const duration = Date.now() - start
-    
-        // Buat response baru dengan menaruh 'took' di bagian atas
-        const responseBody = {
-
-            took: duration, // Waktu eksekusi berada di atas
-
-            ...data // Data asli di bawah
-
-        }
-    
-        originalJson.call(res, responseBody)
-
-    }
-  
-    next()
-
-})
-
 // Contoh route
 app.get('/api/v1', (req, res) => {
 
-    res.json({ status: "OK", code: 200, message: "REQUEST SUCCESS!" , data: null, errors: null })
+    res.json({ status: 200, message: "OK", data: { school: [`http://${HOST}:${PORT}/api/v1/school`, `http://${HOST}:${PORT}/api/v1/school/:id`] } })
+
+})
+
+app.get('/api/v1/school', async (req, res) => {
+
+    try {
+
+        const school = await client.db('kivotos').collection('school').find({}, { projection: { _id: true, name: true } }).toArray()
+
+        if (school) {
+
+            await JSON.stringify(school)
+
+            res.json({ status: 200, message: "OK", data: { school } })
+
+        } else {
+
+            res.json({ status: 404, message: "NOT FOUND", data: null })
+
+        }
+
+    } catch (error) {
+
+        res.json({ status: 500, message: "INTERNAL SERVER ERROR", data: null })
+
+    }
+
+})
+
+app.get('/api/v1/school/:id', async (req, res) => {
+
+    try {
+
+        const id = parseInt(req.params.id)
+
+        const schoolId = await client.db('kivotos').collection('school').findOne({ _id: id })
+
+        if (schoolId) {
+
+            res.json({ status: 200, message: "OK", data: [schoolId] })
+
+        } else {
+
+            res.json({ status: 404, message: "NOT FOUND", data: null })
+
+        }
+
+    } catch (error) {
+
+        res.json({ status: 500, message: "INTERNAL SERVER ERROR", data: null })
+
+    }
 
 })
 
 app.use((req, res, next) => {
 
-    res.json({ status: "NOT FOUND", code: 404, message: "PAGE NOT FOUND!", data: null, errors: ["The requested endpoint was not found"] })
+    res.json({ status: 404, message: "NOT FOUND", data: null })
 
     next()
 
 })
 
 // Jalankan server
-app.listen(3000, () => {
+app.listen(PORT, () => {
 
-    console.log('Server running on port 3000')
+    console.log(`Server is listening at port ${PORT}`)
 
 })
