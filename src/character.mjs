1957b1570ffc161e94f650ecf0289ca58e26e1b1
Fikri Muhammad Fadhilah <179136498+fikrimf280305@users.noreply.github.com>
2024-09-24 17:17:29 +0700

    Add route handler for character

---
+++ src/character.mjs
@@ -0,0 +1,67 @@
+import { client } from './school.mjs'
+
+export const getCharacter = async (req, res) => {
+    try {
+        const character = await client.db('kivotos').collection('character').find({}, {
+            projection: {
+                "_id": true,
+                "firstName": true,
+                "lastName": true
+            }
+        }).toArray()
+
+        if (character) {
+            res.status(200).json({
+                "status": "OK",
+                "code": 200,
+                "data": {
+                    character
+                }
+            })
+        } else {
+            res.status(404).json({
+                "status": "NOT FOUND",
+                "code": 404,
+                "data": null
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            "status": "INTERNAL SERVER ERROR",
+            "code": 500,
+            "data": null
+        })
+    }
+}
+
+export const getCharacterById = async (req, res) => {
+    try {
+        const characterId = await parseInt(req.params.id)
+
+        const character = await client.db('kivotos').collection('character').findOne({
+            "_id": characterId
+        })
+
+        if (character) {
+            res.status(200).json({
+                "status": "OK",
+                "code": 200,
+                "data": [
+                    character
+                ]
+            })
+        } else {
+            res.status(404).json({
+                "status": "NOT FOUND",
+                "code": 404,
+                "data": null
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            "status": "INTERNAL SERVER ERROR",
+            "code": 500,
+            "data": null
+        })
+    }
+}
